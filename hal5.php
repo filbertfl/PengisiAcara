1f8b4d3f3b88e8c50b068563715e7fda7c2365c0
filbertfl <c14200184@john.petra.ac.id>
2022-05-22 19:20:14 +0700

    a

---
+++ hal5.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+
+    <style>
+        .accordion-item {
+            border: 1px solid black;
+        }
+
+        .accordion-item:not(:first-of-type) {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="isi_hal4" class="container">
+        <?php
+        $id_ukm = 1;
+
+        $stmt = $pdo->query("SELECT * FROM `request` WHERE `id_ukm` = $id_ukm");
+        while($row = $stmt->fetch()){
+            $id = $row["id"];
+            $date_time = $row["date_time"];
+            $request_info = $row["request_info"];
+            $status = $row["status"];
+            $id_ukm = $row["id_ukm"];
+
+            $stmt_ukm = $pdo->query("SELECT * FROM `panitia` WHERE id = $id_panitia");
+            while ($row_ukm = $stmt_ukm->fetch()){
+                $nama_ukm = $row_ukm["nama"];
+            } 
+        }
+        ?>
+        <div class="mt-3 accordion" id="accordionExample">
+            <div class="mb-3 accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Request dari Panitia Spetrakuler
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione ex accusantium eum minus magni
+                        nesciunt consequatur nulla, saepe cumque sapiente! Dolor placeat commodi incidunt dolores nam
+                        deleniti voluptatum, perferendis provident?
+                    </div>
+                    <div style="padding: 20px;">
+                        <button type="submit" class="btn btn-success">Accept</button>
+                        <button type="submit" class="btn btn-danger">Deny</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
